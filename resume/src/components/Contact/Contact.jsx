fb8ca740cb7fa8ad88ee13b0086178ef7856425c
vivekchadhuvula01 <vivekchadhuvula@gmail.com>
2024-10-28 21:45:31 +0530

    Contact page is comleted

--- resume/src/components/Contact/Contact.jsx
+++ resume/src/components/Contact/Contact.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-
+import './Contact.css'
+import { IoMdMail } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
 const Contact = () => {
     return (
         <>
             <section className='main-body'>
-                <h2>Projects </h2>
+                <h2>Contact </h2>
                 <span className='line'></span>
             </section>
+            <section className='main'>
+                <p className='text'>Find me on the following Platform</p>
+            </section>
+            <div className='details'>
+                <section className='detail-box-contact'>
+                    <IoMdMail fontSize={'1.3rem'} />
+                    <a href="mailto:vivekchadhuvula@gmail.com">vivekchadhuvula@gmail.com</a>
+                </section>
+                <section className='detail-box-contact'>
+                    <FaLinkedin fontSize={'1.3rem'} />
+                    <a href="https://www.linkedin.com/in/vivek-chadhuvula/" target='_blank'>Linkedin</a>
+                </section>
+            </div>
         </>
     )
 }
